c0a789fc2923722dc142d4d361ecb6e0acd0cb72
Blaz Sovdat <blaz.sovdat@gmail.com>
2017-10-10 11:50:23 +0200

    updates

--- sentinel-2/cby_cloud_detection/script_bcy_original.js
+++ sentinel-2/cby_cloud_detection/script_bcy_original.js
@@ -6,18 +6,19 @@ function clip(a) {
   return Math.max(0, Math.min(1, a));
 }
 
-let bRatio = (B02 - 0.175) / (0.39 - 0.175);
 let NGDR = index(B02, B03);
+let bRatio = (B02 - 0.175) / (0.39 - 0.175);
 let gain = 2.5;
 
-if (B11>0.1 && bRatio > 1) { //cloud
-  var v = 0.5*(bRatio - 1);
-  return [0.5*clip(B04), 0.5*clip(B03), 0.5*clip(B02) + v];
+if (bRatio > 1) { //cloud
+  var v = 0.5 * (bRatio - 1);
+  return [0.5 * clip(B04), 0.5 * clip(B03), 0.5 * clip(B02) + v];
 }
 
-if (B11 > 0.1 && bRatio > 0 && NGDR>0) { //cloud
+if (bRatio > 0 && NGDR>0) { //cloud
   var v = 5 * Math.sqrt(bRatio * NGDR);
   return [0.5 * clip(B04) + v, 0.5 * clip(B03), 0.5 * clip(B02)];
 }
+
 
 return [B04, B03, B02].map(a => gain * a);
