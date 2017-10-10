aabbd2b7c54cddf941b37f74a1796da50b75d27b
Blaz Sovdat <blaz.sovdat@gmail.com>
2017-10-10 10:27:51 +0200

    cleaned code

--- sentinel-2/cby_cloud_detection/script.js
+++ sentinel-2/cby_cloud_detection/script.js
@@ -7,8 +7,8 @@ function clip(a) {
 }
 
 if (bRatio > 1) { //cloud
-  var v = 0.5*(bRatio - 1);
-  return [0.5*clip(B04), 0.5*clip(B03), 0.5*clip(B02) + v];
+  var v = 0.5 * (bRatio - 1);
+  return [0.5 * clip(B04), 0.5 * clip(B03), 0.5 * clip(B02) + v];
 }
 
 if (bRatio > 0 && NGDR>0) { //cloud
@@ -20,4 +20,4 @@ let bRatio = (B02 - 0.175) / (0.39 - 0.175);
 let NGDR = index(B02, B03);
 let gain = 2.5;
 
-return [gain*B04, gain*B03, gain*B02];
+return [B04, B03, B02].map(a => gain * a);
