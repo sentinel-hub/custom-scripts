32788dff3c6b8c93df1fb1f2360aed61f8059032
Blaz Sovdat <blaz.sovdat@gmail.com>
2017-10-24 07:48:32 +0200

    Estimated index bounds; updated colormaps

--- modis/indexdb/id_20.js
+++ modis/indexdb/id_20.js
@@ -2,4 +2,22 @@
 // URL http://www.indexdatabase.de/db/si-single.php?rsindex_id=20=&sensor_id=14
 
 let index = (B06)/(B02);
-return colorBlend(index, [0, 0.2, 0.4, 0.6, 0.8, 1], [[0,0,0], [0.1,0.2,0.5], [0.25,0.4,0.5], [0.4,0.6,0.5], [0.75,0.8,0.5], [1,1,0.5]]);
+let min = 0.059;
+let max = 16.71;
+
+// colorBlend will return a color when the index is between min and max and white when it is less than min.
+// To see black when it is more than max, uncomment the last line of colorBlend.
+// The min/max values were computed automatically and may be poorly specified, feel free to change them to tweak the displayed range.
+
+var underflow_color = [1, 1, 1];
+var low_color = [208/255, 88/255, 126/255];
+var high_color = [241/255, 234/255, 200/255];
+var overflow_color = [0, 0, 0];
+
+return colorBlend(index, [min, min, max],
+[
+	underflow_color,
+	low_color,
+	high_color,
+	//overflow_color // uncomment to see overflows
+]);
