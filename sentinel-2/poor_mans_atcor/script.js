169150e414f2cb1cf336e71f0724c5f5a0f21365
Miha Kadunc <miha.kadunc@sinergise.com>
2017-11-09 13:21:21 +0100

    Compacted functions with arrows, if else blocks with ternary

--- sentinel-2/poor_mans_atcor/script.js
+++ sentinel-2/poor_mans_atcor/script.js
@@ -12,65 +12,52 @@ var debug = false; // Set to 'true' to highlight out-of-range values
 var atmRatios = [1, 2, 3.25]; // Rayleigh-derived consts for automated atmosphere offsets
 
 //== FUNCTIONS ============================
-function sRGBenc(C) {
-  return C < 0.0031308 ? (12.92 * C) : (1.055 * Math.pow(C, 0.41666) - 0.055);
-}
+var sRGBenc = C => C < 0.0031308 ? (12.92 * C) : (1.055 * Math.pow(C, 0.41666) - 0.055);
 
 // atmospheric adjustment
-function atm2p(a, c0, c1) {
-	return (a - c0) / c1;
-}
-function atm1p(a, c0) {
-	return atm2p(a, c0, (1 - c0)**2);
-}
-function atm(a, ii) {
-  if (typeof cManual !== 'undefined') {
-    if (cManual[ii] instanceof Array) {
-      return atm2p(a, cManual[ii][0], cManual[ii][1]);
-    }
-    return atm1p(a, cManual[ii]);
-  }
-  return atm1p(a, c0r * atmRatios[ii]);
-}
+var atm2p = (a, c0, c1) => (a - c0) / c1;
+
+var atm1p = (a, c0) => atm2p(a, c0, (1 - c0)**2);
+
+var atm = (a, ii) => (typeof cManual !== 'undefined')
+    ? (cManual[ii] instanceof Array)
+        ? atm2p(a, cManual[ii][0], cManual[ii][1])
+        : atm1p(a, cManual[ii])
+  : atm1p(a, c0r * atmRatios[ii]);
 
 //contrast enhancement
-function adjFun(a, tx, ty, max) {
+var adjFun = (a, tx, ty, max) => {
   var ar = a / max;
   var txr = tx / max;
   var bot = (2 * txr - 1) * ar - txr;
   return ar * (1 + (txr - ty) * (1 - ar) / bot);
-}
-function adj(a) {
-  return adjFun(a, tx, ty, max);
-}
-function satEnh(rgbArr) {
+};
+
+var adj = a => adjFun(a, tx, ty, max);
+
+var satEnh = rgbArr => {
   var avg = rgbArr.reduce((a, b) => a + b, 0) / rgbArr.length;
   return rgbArr.map(a => avg * (1 - sat) + a * sat);
-}
+};
 
-function checkDebug(arr) {
+var checkDebug = arr => {
   if (!debug) {
     return arr;
   }
   var maxC = Math.max.apply(null, arr);
   var minC = Math.min.apply(null, arr);
   
-  if (minC < 0) { // Highlight too dark pixels
-     return arr.map(a => a < 0 ? 1 : 0);
-  } 
-  if (maxC > 1) { // Highlight too bright pixels
-     if (minC > 1) {
-       return arr.map(a => (a - 1)/(maxC - 1));
-     }
-     return arr.map(a => a > 1 ? 1 : 0);
-  }
-  return arr;
-}
+  return (minC < 0) // Highlight too dark pixels
+     ? arr.map(a => a < 0 ? 1 : 0)
+     : (maxC > 1) // Highlight too bright pixels
+       ? (minC > 1)
+         ? arr.map(a => (a - 1)/(maxC - 1))
+         : arr.map(a => a > 1 ? 1 : 0)
+       : arr;
+};
 
 //== SCRIPT ============================
-var rgb = satEnh(
-	  [B04,B03,B02]
-          .map(atm)
-          .map(adj));
+var rgb = satEnh([B04,B03,B02].map(atm).map(adj));
 		  
 return checkDebug(rgb).map(sRGBenc);
+ 
