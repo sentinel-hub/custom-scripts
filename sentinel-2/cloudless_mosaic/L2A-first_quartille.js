d4eb7648c1881a3bcbe4ad333b260bea3d60d2e9
gmilcinski <grega.milcinski@sinergise.com>
2018-03-16 07:26:11 +0100

    Commiting cloudless mosaic script
    This one was used for New Zealand's mosaic

---
+++ sentinel-2/cloudless_mosaic/L2A-first_quartille.js
@@ -0,0 +1,110 @@
+/*
+Script works on Sentinel-2 L2A data and requires scene classification (SCL) band. 
+It takes one year of data, which is quite compute and time intensive, which is why it is recommended to run it on small area (e.g. 256x256 px).
+An example of the results is New Zealand's cloudless mosaic, available here: https://data.linz.govt.nz/layer/93652-nz-10m-satellite-imagery-2017/
+
+For the output value for each pixel it uses the first quartile value of valid values, each band separately. If there are none it uses invalid values instead. 
+When using SCL its very important to use nearest neighbor resampling with a resolution of about 20m/px or more. 
+*/
+
+function    setup (dss) {
+ setInputComponents([dss.B04,dss.B03,dss.B02,dss.SCL]);
+ setOutputComponentCount(3);
+}
+function filterScenes (scenes, inputMetadata) {
+   return scenes.filter(function (scene) {
+     return scene.date.getTime()>=(inputMetadata.to.getTime()-12*31*24*3600*1000) ;
+   });
+}
+
+function getValue(values) {
+  values.sort( function(a,b) {return a - b;} );
+  return getFirstQuartile(values);
+}
+
+function getFirstQuartile(sortedValues) {
+  var index = Math.floor(sortedValues.length / 4);
+  return sortedValues[index];
+}
+function getDarkestPixel(sortedValues) {
+  return sortedValues[0]; // darkest pixel
+}
+function getSecondDarkestPixel(sortedValues) {
+  if (sortedValues.length > 1) { // second darkest pixel
+     return sortedValues[1];
+  }
+  return sortedValues[0]; // darkest pixel
+}
+
+function validate (samples) {
+ var scl = Math.round(samples.SCL);
+
+ if (scl == 3) { // SC_CLOUD_SHADOW
+   return false;
+ } else if (scl == 9) { // SC_CLOUD_HIGH_PROBA
+   return false;
+ } else if (scl == 8) { // SC_CLOUD_MEDIUM_PROBA
+   return false;
+ } else if (scl == 7) { // SC_CLOUD_LOW_PROBA / UNCLASSIFIED
+   // return false;
+ } else if (scl == 10) { // SC_THIN_CIRRUS
+   return false;
+ } else if (scl == 11) { // SC_SNOW_ICE
+   return false;
+ } else if (scl == 1) { // SC_SATURATED_DEFECTIVE
+   return false;
+ } else if (scl == 2) { // SC_DARK_FEATURE_SHADOW
+   // return false;
+ }
+ return true;
+}
+
+function evaluatePixel(samples, scenes) {
+ var clo_b02 = [];var clo_b03 = [];
+ var clo_b04 = [];var clo_b08 = [];
+ var clo_b02_invalid = [];var clo_b03_invalid = [];
+ var clo_b04_invalid = [];var clo_b08_invalid = [];
+ var a = 0;
+ var a_invalid = 0;
+ var BRIGHTNESS = 1;
+
+ for (var i = 0; i < samples.length; i++) {
+   if (samples[i].B02 > 0 && samples[i].B03 > 0 && samples[i].B04 > 0) {
+
+     var isValid = validate(samples[i]);
+
+     if (isValid) {
+       clo_b02[a] = samples[i].B02;
+       clo_b03[a] = samples[i].B03;
+       clo_b04[a] = samples[i].B04;
+       a = a + 1;
+     } else {
+       clo_b02_invalid[a_invalid] = samples[i].B02;
+       clo_b03_invalid[a_invalid] = samples[i].B03;
+       clo_b04_invalid[a_invalid] = samples[i].B04;
+       a_invalid = a_invalid + 1;
+     }
+   }
+ }
+
+ var rValue;
+ var gValue;
+ var bValue;
+ if (a > 0) {
+   rValue = getValue(clo_b04)*BRIGHTNESS;
+   gValue = getValue(clo_b03)*BRIGHTNESS;
+   bValue = getValue(clo_b02)*BRIGHTNESS;
+ } else if (a_invalid > 0) {
+   rValue = getValue(clo_b04_invalid)*BRIGHTNESS;
+   gValue = getValue(clo_b03_invalid)*BRIGHTNESS;
+   bValue = getValue(clo_b02_invalid)*BRIGHTNESS;
+ } else {
+   rValue = 0;
+   gValue = 0;
+   bValue = 0;
+ }
+
+ return [rValue,
+         gValue,
+         bValue];
+}
