7fb2aba246f0e7742d5826c8025d14909a6d6032
mrepse <marko.repse@sinergise.com>
2018-11-06 14:24:11 +0100

    Update script.js
    improve LAI comments

--- sentinel-2/lai/script.js
+++ sentinel-2/lai/script.js
@@ -1,5 +1,6 @@
-//Leaf area index script as implemented in SNAP
-//visualized as an interval from 0-3. This can be adjusted in evaluatePixel method
+// Leaf Area Index script as implemented in SNAP but without input and output validation!
+// Input/output values which are suspect are not reported or changed. Most values, however, do not fall under this category.
+// Visualized as an interval from 0-3. This can be adjusted in the evaluatePixel method.
 
 var degToRad = Math.PI / 180;
 
