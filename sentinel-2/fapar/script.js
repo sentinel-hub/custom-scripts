bbcb97fd40d2ffcbd8e202527b83f0576697c7b9
mrepse <marko.repse@sinergise.com>
2018-11-30 14:18:53 +0100

    Update script.js

--- sentinel-2/fapar/script.js
+++ sentinel-2/fapar/script.js
@@ -1,7 +1,3 @@
-// FAPAR script as implemented in SNAP but without input and output validation!
-// Input/output values which are suspect are not reported or changed. Most values, however, do not fall under this category.
-// Visualized as an interval from 0-1. This can be adjusted in the evaluatePixel method.
-
 var degToRad = Math.PI / 180;
 
 function evaluatePixel(samples) {
