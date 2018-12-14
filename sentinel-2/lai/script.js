37aa15c0fd6563b83090761e14b35a855af5c005
mrepse <marko.repse@sinergise.com>
2018-12-14 13:47:44 +0100

    spacing

--- sentinel-2/lai/script.js
+++ sentinel-2/lai/script.js
@@ -28,17 +28,17 @@ function evaluatePixel(samples) {
 
 function neuron1(b03_norm,b04_norm,b05_norm,b06_norm,b07_norm,b8a_norm,b11_norm,b12_norm, viewZen_norm,sunZen_norm,relAzim_norm) {
   var sum =
-      4.96238030555279
-    - 0.023406878966470 * b03_norm
-    + 0.921655164636366 * b04_norm
-  	+ 0.135576544080099 * b05_norm
-  	- 1.938331472397950 * b06_norm
-  	- 3.342495816122680 * b07_norm
+	+ 4.96238030555279
+	- 0.023406878966470 * b03_norm
+	+ 0.921655164636366 * b04_norm
+	+ 0.135576544080099 * b05_norm
+	- 1.938331472397950 * b06_norm
+	- 3.342495816122680 * b07_norm
 	+ 0.902277648009576 * b8a_norm
 	+ 0.205363538258614 * b11_norm
-  	- 0.040607844721716 * b12_norm
+	- 0.040607844721716 * b12_norm
 	- 0.083196409727092 * viewZen_norm
-  	+ 0.260029270773809 * sunZen_norm
+	+ 0.260029270773809 * sunZen_norm
 	+ 0.284761567218845 * relAzim_norm;
 	
   return tansig(sum);
@@ -46,17 +46,17 @@ function neuron1(b03_norm,b04_norm,b05_norm,b06_norm,b07_norm,b8a_norm,b11_norm,
 
 function neuron2(b03_norm,b04_norm,b05_norm,b06_norm,b07_norm,b8a_norm,b11_norm,b12_norm, viewZen_norm,sunZen_norm,relAzim_norm) {
   var sum =
-      1.4160084439815
-    - 0.132555480856684 * b03_norm
-    - 0.139574837333540 * b04_norm
-  	- 1.014606016898920 * b05_norm
-  	- 1.330890038649270 * b06_norm
-  	+ 0.031730624503341 * b07_norm
+	+ 1.416008443981500
+	- 0.132555480856684 * b03_norm
+	- 0.139574837333540 * b04_norm
+	- 1.014606016898920 * b05_norm
+	- 1.330890038649270 * b06_norm
+	+ 0.031730624503341 * b07_norm
 	- 1.433583541317050 * b8a_norm
 	- 0.959637898574699 * b11_norm
-  	+ 1.133115706551000 * b12_norm
+	+ 1.133115706551000 * b12_norm
 	+ 0.216603876541632 * viewZen_norm
-  	+ 0.410652303762839 * sunZen_norm
+	+ 0.410652303762839 * sunZen_norm
 	+ 0.064760155543506 * relAzim_norm;
 	
   return tansig(sum);
@@ -64,17 +64,17 @@ function neuron2(b03_norm,b04_norm,b05_norm,b06_norm,b07_norm,b8a_norm,b11_norm,
 
 function neuron3(b03_norm,b04_norm,b05_norm,b06_norm,b07_norm,b8a_norm,b11_norm,b12_norm, viewZen_norm,sunZen_norm,relAzim_norm) {
   var sum =
-      1.07589704721331
-    + 0.086015977724868 * b03_norm
-    + 0.616648776881434 * b04_norm
-  	+ 0.678003876446556 * b05_norm
-  	+ 0.141102398644968 * b06_norm
-  	- 0.096682206883546 * b07_norm
+	+ 1.075897047213310
+	+ 0.086015977724868 * b03_norm
+	+ 0.616648776881434 * b04_norm
+	+ 0.678003876446556 * b05_norm
+	+ 0.141102398644968 * b06_norm
+	- 0.096682206883546 * b07_norm
 	- 1.128832638862200 * b8a_norm
 	+ 0.302189102741375 * b11_norm
-  	+ 0.434494937299725 * b12_norm
+	+ 0.434494937299725 * b12_norm
 	- 0.021903699490589 * viewZen_norm
-  	- 0.228492476802263 * sunZen_norm
+	- 0.228492476802263 * sunZen_norm
 	- 0.039460537589826 * relAzim_norm;
 	
   return tansig(sum);
@@ -82,17 +82,17 @@ function neuron3(b03_norm,b04_norm,b05_norm,b06_norm,b07_norm,b8a_norm,b11_norm,
 
 function neuron4(b03_norm,b04_norm,b05_norm,b06_norm,b07_norm,b8a_norm,b11_norm,b12_norm, viewZen_norm,sunZen_norm,relAzim_norm) {
   var sum =
-      1.533988264655420
-    - 0.109366593670404 * b03_norm
-    - 0.071046262972729 * b04_norm
+	+ 1.533988264655420
+	- 0.109366593670404 * b03_norm
+	- 0.071046262972729 * b04_norm
 	+ 0.064582411478320 * b05_norm
- 	+ 2.906325236823160 * b06_norm
-  	- 0.673873108979163 * b07_norm
+	+ 2.906325236823160 * b06_norm
+	- 0.673873108979163 * b07_norm
 	- 3.838051868280840 * b8a_norm
 	+ 1.695979344531530 * b11_norm
-  	+ 0.046950296081713 * b12_norm
+	+ 0.046950296081713 * b12_norm
 	- 0.049709652688365 * viewZen_norm
-  	+ 0.021829545430994 * sunZen_norm
+	+ 0.021829545430994 * sunZen_norm
 	+ 0.057483827104091 * relAzim_norm;
 	
   return tansig(sum);
@@ -100,17 +100,17 @@ function neuron4(b03_norm,b04_norm,b05_norm,b06_norm,b07_norm,b8a_norm,b11_norm,
 
 function neuron5(b03_norm,b04_norm,b05_norm,b06_norm,b07_norm,b8a_norm,b11_norm,b12_norm, viewZen_norm,sunZen_norm,relAzim_norm) {
   var sum =
-      3.02411593075723
-    - 0.089939416159969 * b03_norm
-    + 0.175395483106147 * b04_norm
-  	- 0.081847329172620 * b05_norm
+	+ 3.024115930757230
+	- 0.089939416159969 * b03_norm
+	+ 0.175395483106147 * b04_norm
+	- 0.081847329172620 * b05_norm
 	+ 2.219895367487790 * b06_norm
-  	+ 1.713873975136850 * b07_norm
+	+ 1.713873975136850 * b07_norm
 	+ 0.713069186099534 * b8a_norm
 	+ 0.138970813499201 * b11_norm
-  	- 0.060771761518025 * b12_norm
+	- 0.060771761518025 * b12_norm
 	+ 0.124263341255473 * viewZen_norm
-  	+ 0.210086140404351 * sunZen_norm
+	+ 0.210086140404351 * sunZen_norm
 	- 0.183878138700341 * relAzim_norm;
 	
   return tansig(sum);
@@ -118,11 +118,11 @@ function neuron5(b03_norm,b04_norm,b05_norm,b06_norm,b07_norm,b8a_norm,b11_norm,
 
 function layer2(neuron1, neuron2, neuron3, neuron4, neuron5) {
   var sum =
-      1.096963107077220
+	+ 1.096963107077220
 	- 1.500135489728730 * neuron1
-  	- 0.096283269121503 * neuron2
-  	- 0.194935930577094 * neuron3
-  	- 0.352305895755591 * neuron4
+	- 0.096283269121503 * neuron2
+	- 0.194935930577094 * neuron3
+	- 0.352305895755591 * neuron4
 	+ 0.075107415847473 * neuron5;
 
   return sum;
