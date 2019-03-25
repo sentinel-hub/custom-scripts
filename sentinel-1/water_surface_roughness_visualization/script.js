10c743b7f2acef6940d8af42af19a7f53a5cd765
gmilcinski <grega.milcinski@sinergise.com>
2019-03-25 07:21:12 +0100

    Water Surface Roughness Visualization  script

---
+++ sentinel-1/water_surface_roughness_visualization/script.js
@@ -0,0 +1,6 @@
+// Water Surface Roughness Visualization 
+// Author: Annamaria Luongo (Twitter: @annamaria_84, http://www.linkedin.com/in/annamaria-luongo-RS) 
+// License: CC BY 4.0 International - https://creativecommons.org/licenses/by/4.0/ 
+
+val = Math.log(0.05/(0.018+VV*1.5)); 
+return [val]; 
