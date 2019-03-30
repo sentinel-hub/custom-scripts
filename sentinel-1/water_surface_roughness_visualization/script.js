0c2441ba53e8ee371919405c994b115164794016
gmilcinski <grega.milcinski@sinergise.com>
2019-03-30 19:58:53 +0100

    minor revision

--- sentinel-1/water_surface_roughness_visualization/script.js
+++ sentinel-1/water_surface_roughness_visualization/script.js
@@ -2,5 +2,5 @@
 // Author: Annamaria Luongo (Twitter: @annamaria_84, http://www.linkedin.com/in/annamaria-luongo-RS) 
 // License: CC BY 4.0 International - https://creativecommons.org/licenses/by/4.0/ 
 
-val = Math.log(0.05/(0.018+VV*1.5)); 
+var val = Math.log(0.05/(0.018+VV*1.5)); 
 return [val]; 
