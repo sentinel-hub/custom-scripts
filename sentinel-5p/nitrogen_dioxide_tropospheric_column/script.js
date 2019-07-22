67508be5d5708c5fa73352b30198dc6f197ac9c3
gmilcinski <grega.milcinski@sinergise.com>
2019-07-22 12:51:53 +0200

    Updating names of the variables to prevent override

--- sentinel-5p/nitrogen_dioxide_tropospheric_column/script.js
+++ sentinel-5p/nitrogen_dioxide_tropospheric_column/script.js
@@ -2,9 +2,9 @@
 // Author: Annamaria Luongo (Twitter: @annamaria_84, www.linkedin.com/in/annamaria-luongo-RS)
 // CC BY 4.0 International - https://creativecommons.org/licenses/by/4.0/
 
-var NO2_values = [ 2.5*1e-5, 5*1e-5, 7.5*1e-5, 1*1e-4,  2.5*1e-4, 5*1e-4]; //value of NO2 in mol/m2
+var values = [ 2.5*1e-5, 5*1e-5, 7.5*1e-5, 1*1e-4,  2.5*1e-4, 5*1e-4]; //value of NO2 in mol/m2
 
-return colorBlend(NO2, NO2_values,
+return colorBlend(NO2, values,
    [
     [ 64/255,  27/255, 134/255], //  -> 2.5*1e-5 = #401B86
     [ 37/255,  70/255, 201/255], //  -> 5.0*1e-5 = #2546C9
