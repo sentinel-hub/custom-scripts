dc4b8590b6a71ce81e390755d0619b8f3228d469
Monja Šebela <monja.sebela@sinergise.com>
2019-12-16 13:42:46 +0100

    Fixing ARI description and script, adding images and adding/modifying README files and imags of burned_area script, max_ndvi, msi, ndci and red_edge_position

--- sentinel-2/ari/script.js
+++ sentinel-2/ari/script.js
@@ -4,29 +4,6 @@
 // General formula: 1/550nm-1/700nm
 //
 // URL https://www.indexdatabase.de/db/si-single.php?sensor_id=96&rsindex_id=214
-//
 
 let index = 1.0 / B03 - 1.0 / B05;
-let min = -64.572;
-let max = 64.768;
-let zero = 0.0;
-
-// colorBlend will return a color when the index is between min and max and white when it is less than min.
-// To see black when it is more than max, uncomment the last line of colorBlend.
-// The min/max values were computed automatically and may be poorly specified, feel free to change them to tweak the displayed range.
-// This index crosses zero, so a diverging color map is used. To tweak the value of the break in the color map, change the variable 'zero'.
-
-let underflow_color = [1, 1, 1];
-let low_color = [208/255, 88/255, 126/255];
-let high_color = [241/255, 234/255, 200/255];
-let zero_color = [0, 147/255, 146/255];
-let overflow_color = [0, 0, 0];
-
-return colorBlend(index, [min, min, zero, max],
-[
-	underflow_color,
-	low_color,
-	zero_color, // divergent step at zero
-	high_color,
-	//overflow_color // uncomment to see overflows
-]);
+return [index]
