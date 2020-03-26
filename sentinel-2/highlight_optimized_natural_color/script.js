d445768672657269cd048812df43c9cc6b108e43
MonjaSebela <54274296+MonjaSebela@users.noreply.github.com>
2020-03-26 17:51:23 +0100

    Update script.js

--- sentinel-2/highlight_optimized_natural_color/script.js
+++ sentinel-2/highlight_optimized_natural_color/script.js
@@ -8,7 +8,7 @@ return [Math.cbrt(0.6*B04 - 0.035),
 		
 		
 /*For S2L2A:
-return [Math.cbrt(0.6*B04 - 0.035),
-        Math.cbrt(0.6*B03 - 0.035),
-        Math.cbrt(0.6*B02 - 0.035)]	
+return [Math.cbrt(0.6*B04),
+        Math.cbrt(0.6*B03),
+        Math.cbrt(0.6*B02)]	
 /*
