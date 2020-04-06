0fb38c9f7cf9648ea1d0bcc4267273da734097e9
MonjaSebela <54274296+MonjaSebela@users.noreply.github.com>
2020-04-06 04:31:08 +0200

    Update script.js

--- airbus_pleiades/true_color/script.js
+++ airbus_pleiades/true_color/script.js
@@ -3,11 +3,11 @@
 
 function setup() {
   return {
-    input: ["B1", "B2", "B3"],
+    input: ["B0", "B1", "B2"],
     output: { bands: 3 }
   };
 }
 
 function evaluatePixel(sample) {
-  return [sample.B3/3000, sample.B2/3000, sample.B1/3000];
+  return [sample.B2/3000, sample.B1/3000, sample.B0/3000];
 }
