c299974fdbf220d02a97e359f65b4129ec971315
Dorothy Rono <ronodorothy@yahoo.com>
2020-06-04 19:03:40 +0200

    add visualization to SAVI

--- sentinel-2/savi/script.js
+++ sentinel-2/savi/script.js
@@ -3,27 +3,15 @@
 // URL https://www.indexdatabase.de/db/si-single.php?sensor_id=96&rsindex_id=87
 // Initialize parameters
 
-let L = 0.428;
-let index = (B08 - B04) / (B08 + B04 + L) * (1.0 + L);
+let L = 0.428; // L = soil brightness correction factor could range from (0 -1)
+let index = (B08 - B04) / (B08 + B04 + L) * (1.0 + L); // calculate savi index
 
-/*
-// using colorblend visualization  in EO browser 
-return colorBlend   // Start of colorBlend function
-(index,	            //  specify the index
-     [ 0,0.1, 0.6, 1], //  specify the index borders
-     [ [0.69,0.88,0.901],   // specify RGB colors 
-       [0.74,0.72,0.42],
-       [0, 0.39, 0], 
-       [0, 0.50, 0],
-     ]
-);
-*/
 
 //using if statements visualization  in EO Browser
-if (index <-1) return [0,0,0];
-else if (index <0) return [0.69,0.88,0.901];
-else if (index <0.09) return [0.74,0.72,0.42];
-else if (index <0.1) return [0.64,0.8,0.35];
+if (index <-1) return [0,0,0]; // black color for values less than -1
+else if (index <0) return [0.69,0.88,0.901]; // blue color majorly water
+else if (index <0.09) return [0.74,0.72,0.42];  // yellow
+else if (index <0.1) return [0.64,0.8,0.35]; 
 else if (index <0.2) return [0.57,0.75,0.32];
 else if (index <0.3) return [0.6,0.8,0.19];
 else if (index <0.4) return [0,0.50,0];
@@ -31,5 +19,18 @@ else if (index <0.5) return [0,0.39,0];
 else if (index <0.6) return [0.17,0.41,0.18];
 else if (index <0.7) return [0.18,0.54,0.34];
 else if (index <0.8) return [0.19,0.43,0.11];
-else if (index <1)   return [0.23,0.70,0.44];
+else if (index <1)   return [0.23,0.70,0.44]; // green
 else return [1,1,1];
+
+/*
+// using colorblend visualization  in EO browser 
+return colorBlend   // Start of colorBlend function
+(index,	            //  specify the index
+     [ 0,0.1, 0.6, 1], //  specify the borders
+     [ [0.69,0.88,0.901],   // specify RGB colors 
+       [0.74,0.72,0.42],
+       [0, 0.39, 0], 
+       [0, 0.50, 0],
+     ]
+);
+*/
