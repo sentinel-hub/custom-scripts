d475c990596f7f290fc43eb2507a721ec73f85f3
Dorothy Rono <ronodorothy@yahoo.com>
2020-06-04 18:28:49 +0200

    added visualization scheme to SAVI

--- sentinel-2/savi/script.js
+++ sentinel-2/savi/script.js
@@ -5,4 +5,31 @@
 
 let L = 0.428;
 let index = (B08 - B04) / (B08 + B04 + L) * (1.0 + L);
-return[index]
+
+/*
+// using colorblend visualization  in EO browser 
+return colorBlend   // Start of colorBlend function
+(index,	            //  specify the index
+     [ 0,0.1, 0.6, 1], //  specify the index borders
+     [ [0.69,0.88,0.901],   // specify RGB colors 
+       [0.74,0.72,0.42],
+       [0, 0.39, 0], 
+       [0, 0.50, 0],
+     ]
+);
+*/
+
+//using if statements visualization  in EO Browser
+if (index <-1) return [0,0,0];
+else if (index <0) return [0.69,0.88,0.901];
+else if (index <0.09) return [0.74,0.72,0.42];
+else if (index <0.1) return [0.64,0.8,0.35];
+else if (index <0.2) return [0.57,0.75,0.32];
+else if (index <0.3) return [0.6,0.8,0.19];
+else if (index <0.4) return [0,0.50,0];
+else if (index <0.5) return [0,0.39,0];
+else if (index <0.6) return [0.17,0.41,0.18];
+else if (index <0.7) return [0.18,0.54,0.34];
+else if (index <0.8) return [0.19,0.43,0.11];
+else if (index <1)   return [0.23,0.70,0.44];
+else return [1,1,1];
