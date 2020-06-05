0333d9bf9e70ab9d48c95763eb14ced8e7d6c9ed
Dorothy Rono <dorothy.rono@sinergise.com>
2020-06-05 16:35:40 +0200

    added savi visualization

--- sentinel-2/savi/script.js
+++ sentinel-2/savi/script.js
@@ -6,31 +6,32 @@
 let L = 0.428; // L = soil brightness correction factor could range from (0 -1)
 let index = (B08 - B04) / (B08 + B04 + L) * (1.0 + L); // calculate savi index
 
-
-//using if statements visualization  in EO Browser
-if (index <-1) return [0,0,0]; // black color for values less than -1
-else if (index <0) return [0.69,0.88,0.901]; // blue color majorly water
-else if (index <0.09) return [0.74,0.72,0.42];  // yellow
-else if (index <0.1) return [0.64,0.8,0.35]; 
-else if (index <0.2) return [0.57,0.75,0.32];
-else if (index <0.3) return [0.6,0.8,0.19];
-else if (index <0.4) return [0,0.50,0];
-else if (index <0.5) return [0,0.39,0];
-else if (index <0.6) return [0.17,0.41,0.18];
-else if (index <0.7) return [0.18,0.54,0.34];
-else if (index <0.8) return [0.19,0.43,0.11];
-else if (index <1)   return [0.23,0.70,0.44]; // green
-else return [1,1,1];
-
-/*
 // using colorblend visualization  in EO browser 
 return colorBlend   // Start of colorBlend function
 (index,	            //  specify the index
-     [ 0,0.1, 0.6, 1], //  specify the borders
-     [ [0.69,0.88,0.901],   // specify RGB colors 
+     [ 0,0.1, 0.2,0.4, 0.5,0.7,1], //  specify the borders
+     [ [0.69,0.88,0.90],   // specify RGB colors 
        [0.74,0.72,0.42],
-       [0, 0.39, 0], 
+       [0.60,0.80,0.20],
+       [0.13, 0.54, 0.13],
        [0, 0.50, 0],
+       [0, 0.39, 0],
+       [0, 0.29, 0],
      ]
 );
+
+/*
+//using if statements visualization  in EO Browser
+if (index <-1) return [1,1,1]; // white color for values less than -1
+else if (index <0) return [0.69,0.88,0.901]; // blue color majorly water
+else if (index <0.09) return [0.74,0.72,0.42];  
+else if (index <0.1) return [0.64,0.8,0.35]; 
+else if (index <0.2) return [0.57,0.75,0.32];
+else if (index <0.3) return [0.60,0.80,0.19];
+else if (index <0.4) return [0.13,0.63,0.13];
+else if (index <0.5) return [0.13,0.55,0.13];
+else if (index <0.6) return [0,0.50,0];
+else if (index <0.7) return [0,0.39,0];
+else if (index <1) return   [0, 0.29,0]; 
+else return [0,0,0];
 */
