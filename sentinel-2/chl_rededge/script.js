70faa598ca9568fdd533b0a389eb3e7cdc7c4ede
Monja Šebela <monja.sebela@sinergise.com>
2020-07-14 14:14:01 +0200

    Chlorophyl rededge visualization improvement

--- sentinel-2/chl_rededge/script.js
+++ sentinel-2/chl_rededge/script.js
@@ -7,22 +7,13 @@
 //
 
 let index = Math.pow((B07 / B05), (-1.0));
-let min = 0.058;
-let max = 17.149;
 
-// colorBlend will return a color when the index is between min and max and white when it is less than min.
-// To see black when it is more than max, uncomment the last line of colorBlend.
-// The min/max values were computed automatically and may be poorly specified, feel free to change them to tweak the displayed range.
-
-let underflow_color = [1, 1, 1];
-let low_color = [208/255, 88/255, 126/255];
-let high_color = [241/255, 234/255, 200/255];
-let overflow_color = [0, 0, 0];
-
-return colorBlend(index, [min, min, max],
+return colorBlend(index, [0, 0.2, 0.5, 0.7, 1, 5],
 [
-	underflow_color,
-	low_color,
-	high_color,
-	//overflow_color // uncomment to see overflows
+  [0,0,0],
+  [0,0.5,0],
+  [0.2,0.8,0],
+  [1,1,0],
+  [0.8,0.8,0.8],
+  [1,1,1]
 ]);
