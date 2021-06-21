a54cb06868fd5e3531a448f4f0ac8370f6d08fae
MonjaSebela <54274296+MonjaSebela@users.noreply.github.com>
2021-06-21 11:45:15 +0200

    Update script.js

--- sentinel-2/aesthetic-neon/script.js
+++ sentinel-2/aesthetic-neon/script.js
@@ -13,7 +13,7 @@ function evaluatePixel(sample) {
   
   let val = sample.B12 - sample.B04
   let gain = 2.3
-  let gamma = - 0.95 // - 0.75
+  let gamma = - 0.95 // recommended gamma: -0.55 to -0.95
   return [(gain * 3 * val) + gamma,
           (gain * 2.5 * sample.B03) + gamma,
           (gain * 4.2 * sample.B02) + gamma,
