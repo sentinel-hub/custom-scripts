3c210fbf841bfc1a8d00afa2670693f0cc95f03c
MonjaSebela <54274296+MonjaSebela@users.noreply.github.com>
2021-06-21 11:34:08 +0200

    Update script.js

--- sentinel-2/aesthetic-neon/script.js
+++ sentinel-2/aesthetic-neon/script.js
@@ -11,9 +11,11 @@ function setup() {
 
 function evaluatePixel(sample) {
   
-  let val = sample.B12-sample.B04
-  return [(3 * val)-0.35,
-          (2.5 * sample.B03)-0.35,
-          (4.2 * sample.B02)-0.35,
+  let val = sample.B12 - sample.B04
+  let gain = 2.3
+  let gamma = - 0.95 // - 0.75
+  return [(gain * 3 * val) + gamma,
+          (gain * 2.5 * sample.B03) + gamma,
+          (gain * 4.2 * sample.B02) + gamma,
           sample.dataMask ];
 }
