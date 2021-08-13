56ee9c5980d9bb232733058bb4b1007fa9a7b749
MonjaSebela <54274296+MonjaSebela@users.noreply.github.com>
2021-08-13 09:53:06 +0200

    Update script.js
    swir band correction

--- landsat-4-5-tm/swir/script.js
+++ landsat-4-5-tm/swir/script.js
@@ -1,4 +1,4 @@
-//VERSION=3
+  //VERSION=3
 let minVal = 0.0;
 let maxVal = 0.4;
 
@@ -6,12 +6,12 @@ let viz = new HighlightCompressVisualizer(minVal, maxVal);
 
 function setup() {
   return {
-    input: ["B07", "B06", "B04","dataMask"],
+    input: ["B07", "B05", "B03","dataMask"],
     output: { bands: 4 }
   };
 }
 
 function evaluatePixel(samples) {
-    let val = [samples.B07, samples.B06, samples.B04,samples.dataMask];
+    let val = [samples.B07, samples.B05, samples.B03,samples.dataMask];
     return viz.processList(val);
 }
