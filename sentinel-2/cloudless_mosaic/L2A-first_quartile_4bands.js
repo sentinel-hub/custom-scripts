72f293f38a81d11b9206b03a5fc53df1a6ff9fee
MonjaSebela <54274296+MonjaSebela@users.noreply.github.com>
2021-11-03 15:07:21 +0100

    Moving *1000 to the return statement

--- sentinel-2/cloudless_mosaic/L2A-first_quartile_4bands.js
+++ sentinel-2/cloudless_mosaic/L2A-first_quartile_4bands.js
@@ -67,26 +67,21 @@ function evaluatePixel(samples, scenes) {
   
   for (var i = 0; i < samples.length; i++) {
     var sample = samples[i];
-    
-    let B04 = sample.B04 * 10000
-    let B03 = sample.B03 * 10000
-    let B02 = sample.B02 * 10000
-    let B08 = sample.B08 * 10000
 
-    if (B02 > 0 && B03 > 0 && B04 > 0 && B08 > 0) {
+    if (sample.B02 > 0 && sample.B03 > 0 && sample.B04 > 0 && sample.B08 > 0) {
       var isValid = validate(sample);
       
       if (isValid) {
-        clo_b02[a] = B02;
-        clo_b03[a] = B03;
-        clo_b04[a] = B04;
-        clo_b08[a] = B08;
+        clo_b02[a] = sample.B02;
+        clo_b03[a] = sample.B03;
+        clo_b04[a] = sample.B04;
+        clo_b08[a] = sample.B08;
         a = a + 1;
       } else {
-        clo_b02_invalid[a_invalid] = B02;
-        clo_b03_invalid[a_invalid] = B03;
-        clo_b04_invalid[a_invalid] = B04;
-        clo_b08_invalid[a_invalid] = B08;
+        clo_b02_invalid[a_invalid] = sample.B02;
+        clo_b03_invalid[a_invalid] = sample.B03;
+        clo_b04_invalid[a_invalid] = sample.B04;
+        clo_b08_invalid[a_invalid] = sample.B08;
         a_invalid = a_invalid + 1;
       }
     }
@@ -112,5 +107,8 @@ function evaluatePixel(samples, scenes) {
     bValue = 0;
     nValue = 0;
   }
-  return [rValue, gValue, bValue, nValue]
+  return [rValue * 10000, 
+          gValue * 10000, 
+          bValue * 10000, 
+          nValue * 10000]
 }
