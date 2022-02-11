8b69edd14c51fbaa613b7885b681ec2c4da0905b
Dorothyrono <ronodorothy@yahoo.com>
2022-02-11 17:04:12 +0100

    return dataMask

--- sentinel-1/urban_areas/script.js
+++ sentinel-1/urban_areas/script.js
@@ -1,16 +1,8 @@
-
+      
 /*URBAN AREAS
   Sentinel1-AWS-IW-VVVH
   Author: Monja Sebela
 */
-//VERSION=3
-function setup() {
-  return {
-    input: ["VV", "VH", "dataMask"],
-    output: { bands: 4 }
-  }
-}
 
-function evaluatePixel(sample) {
-  return [5.5 * sample.VH > 0.5, sample.VV, sample.VH * 8, sample.dataMask]
-} 
+
+return [5.5 * VH > 0.5, VV, VH * 8, dataMask];
