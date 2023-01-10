4165b7e32e66319c0fa1f93951a44c1cc5653bc7
wray <william.ray@sinergise.com>
2023-01-10 10:23:28 +0100

    evalscript refactored

--- landsat-8/bqa/script.js
+++ landsat-8/bqa/script.js
@@ -3,52 +3,34 @@
 function setup() {
   return {
     input: ["B02", "B03", "B04", "BQA"],
-    output: { bands: 3, sampleType: "UINT8" }
+    output: { bands: 3, sampleType: "UINT8" },
   };
 }
-  
+
 function evaluatePixel(sample) {
+  // define the decoder function
 
-// decoding BQA classes  
-var bqa = decodeL8C2Qa(sample.BQA)
+  var bqa = decodeL8C2Qa(sample.BQA);
 
-var water_pixels = bqa.water
-var snow_pixels = bqa.snow
-var clouds = bqa.cloud
-var cloud_dilated = bqa.dilatedCloud
-var cloudshadows = bqa.cloudShadow
-var cirrus_clouds = bqa.cirrus
-var fill_pixels = bqa.fill
-var clear_pixels = bqa.clear
+  // defining the colormap
 
-// defining the colormap
-
-
-if (water_pixels == 1){
-  return [0, 0, 204]
-}
-else if (snow_pixels == 1) {
-  return [255, 0, 255]
-}
-else if (clouds == 1) {
-  return [225, 225, 225]
-}
-else if (cloud_dilated == 1) {
-  return [175, 175, 175]
-}
-else if (cloudshadows == 1) {
-  return [153, 102, 51]
-}
-else if (cirrus_clouds == 1) {
-  return [102, 255, 255]
-}
-else if (fill_pixels == 1) {
-  return [255, 0, 0]
-}
-else if (clear_pixels == 1) {
-  return [1000*sample.B04, 1000*sample.B03, 1000*sample.B02]
-}
-else{
-  return [0, 0, 0]
-}
+  if (bqa.water == 1) {
+    return [0, 0, 204];
+  } else if (bqa.snow == 1) {
+    return [255, 0, 255];
+  } else if (bqa.cloud == 1) {
+    return [225, 225, 225];
+  } else if (bqa.dilatedCloud == 1) {
+    return [175, 175, 175];
+  } else if (bqa.cloudShadow == 1) {
+    return [153, 102, 51];
+  } else if (bqa.cirrus == 1) {
+    return [102, 255, 255];
+  } else if (bqa.fill == 1) {
+    return [255, 0, 0];
+  } else if (bqa.clear == 1) {
+    return [1000 * sample.B04, 1000 * sample.B03, 1000 * sample.B02];
+  } else {
+    return [0, 0, 0];
+  }
 }
