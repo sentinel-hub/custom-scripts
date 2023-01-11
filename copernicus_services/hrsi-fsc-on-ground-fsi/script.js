78fbd449f60593bef02b4c035a8f5f08dd79c9e8
wray <william.ray@sinergise.com>
2023-01-11 09:09:08 +0100

    evalscripts reformatted

--- copernicus_services/hrsi-fsc-on-ground-fsi/script.js
+++ copernicus_services/hrsi-fsc-on-ground-fsi/script.js
@@ -3,29 +3,24 @@
 function setup() {
   return {
     input: ["FSCOG", "dataMask"],
-    output: {bands: 4,
-    sampleType: "AUTO"
-  }
-}
+    output: { bands: 4, sampleType: "AUTO" },
+  };
 }
 
 const ramps = [
   [0, 0xffffff],
-  [100, 0x4d4dff ],
+  [100, 0x4d4dff],
 ];
 
 const visualizer = new ColorRampVisualizer(ramps);
 
 function evaluatePixel(sample) {
-
-var FSCOG_viz = visualizer.process(sample.FSCOG)
-  if (sample.FSCOG == 255){
-    return [0,0,0, sample.dataMask]
+  var FSCOG_viz = visualizer.process(sample.FSCOG);
+  if (sample.FSCOG == 255) {
+    return [0, 0, 0, sample.dataMask];
+  } else if (sample.FSCOG == 205) {
+    return [0.9, 0.9, 0.9, sample.dataMask];
+  } else {
+    return [FSCOG_viz[0], FSCOG_viz[1], FSCOG_viz[2], sample.dataMask];
   }
-  else if (sample.FSCOG == 205){
-    return [0.9, 0.9, 0.9, sample.dataMask]
-  }
-  else {
-  return [FSCOG_viz[0], FSCOG_viz[1], FSCOG_viz[2], sample.dataMask]
-}  
-}  
+}
--- copernicus_services/hrsi-gfsc-fsc/script.js
+++ copernicus_services/hrsi-gfsc-fsc/script.js
@@ -3,29 +3,24 @@
 function setup() {
   return {
     input: ["GF", "dataMask"],
-    output: {bands: 4,
-    sampleType: "AUTO"
-  }
-}
+    output: { bands: 4, sampleType: "AUTO" },
+  };
 }
 
 const ramps = [
   [0, 0xffffff],
-  [100, 0x4d4dff ],
+  [100, 0x4d4dff],
 ];
 
 const visualizer = new ColorRampVisualizer(ramps);
 
 function evaluatePixel(sample) {
-
-var GF_viz = visualizer.process(sample.GF)
-  if (sample.GF == 255){
-    return [0,0,0, sample.dataMask]
+  var GF_viz = visualizer.process(sample.GF);
+  if (sample.GF == 255) {
+    return [0, 0, 0, sample.dataMask];
+  } else if (sample.GF == 205) {
+    return [0.9, 0.9, 0.9, sample.dataMask];
+  } else {
+    return [GF_viz[0], GF_viz[1], GF_viz[2], sample.dataMask];
   }
-  else if (sample.GF == 205){
-    return [0.9, 0.9, 0.9, sample.dataMask]
-  }
-  else {
-  return [GF_viz[0], GF_viz[1], GF_viz[2], sample.dataMask]
-}  
-}  
+}
