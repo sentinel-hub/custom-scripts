aea1c4a9a9bd78bf2ae40da5930c42dfab1919bd
wray <william.ray@sinergise.com>
2023-01-11 09:09:56 +0100

    ndsi reformatted

--- copernicus_services/hrsi-fsc-ndsi/script.js
+++ copernicus_services/hrsi-fsc-ndsi/script.js
@@ -3,29 +3,24 @@
 function setup() {
   return {
     input: ["NDSI", "dataMask"],
-    output: {bands: 4,
-    sampleType: "AUTO"
-  }
-}
+    output: { bands: 4, sampleType: "AUTO" },
+  };
 }
 
 const ramps = [
   [0, 0xffffff],
-  [100, 0x4d4dff ],
+  [100, 0x4d4dff],
 ];
 
 const visualizer = new ColorRampVisualizer(ramps);
 
 function evaluatePixel(sample) {
-
-var NDSI_viz = visualizer.process(sample.NDSI)
-  if (sample.NDSI == 255){
-    return [0,0,0, sample.dataMask]
+  var NDSI_viz = visualizer.process(sample.NDSI);
+  if (sample.NDSI == 255) {
+    return [0, 0, 0, sample.dataMask];
+  } else if (sample.NDSI == 205) {
+    return [0.9, 0.9, 0.9, sample.dataMask];
+  } else {
+    return [NDSI_viz[0], NDSI_viz[1], NDSI_viz[2], sample.dataMask];
   }
-  else if (sample.NDSI == 205){
-    return [0.9, 0.9, 0.9, sample.dataMask]
-  }
-  else {
-  return [NDSI_viz[0], NDSI_viz[1], NDSI_viz[2], sample.dataMask]
-}  
-}  
+}
