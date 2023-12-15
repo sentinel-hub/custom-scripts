6740505175920ecb8d8b7d4154dc570831fdeafa
Christoph Paulik <christoph@fqxgftjfpp.kpn>
2023-12-15 10:13:28 +0100

    Add Crop Biomass Evalscripts

---
+++ planetary-variables/crop-biomass/raw.js
@@ -0,0 +1,13 @@
+//VERSION=3
+function setup() {
+  return {
+    input: ["CB"],
+    output: { bands: 1, sampleType: 'FLOAT32' }
+  };
+}
+
+let factor = 1/1000;
+function evaluatePixel(sample) {
+	return [sample.CH*factor]
+    };
+
