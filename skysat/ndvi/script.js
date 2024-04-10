a870a91b140ce358ac69017149eca1b047f96642
Ziga Cernigoj <ziga.cernigoj@sinergise.com>
2024-04-10 13:46:20 +0200

    PlanetScope NDVI: update script.js

--- skysat/ndvi/script.js
+++ skysat/ndvi/script.js
@@ -1,14 +1,18 @@
 //VERSION=3
+// NDVI
+
 function setup() {
   return {
-    input: ["red", "nir", "dataMask"],
-    output: { bands: 4 }
+    input: ["Red", "NIR", "dataMask"],
+    output: [
+      { id: "default", bands: 4 },
+    ]
   }
 }
-var f = 10000
+
 function evaluatePixel(sample) {
-  var NDVI = index(sample.nir, sample.red)
-  return valueInterpolate(NDVI,
+  let NDVI = index(sample.NIR, sample.Red);
+  let id_default = valueInterpolate(NDVI,
     [0, 0.2, 0.3, 0.4, 0.5, 1.0],
     [
       [1, 1, 0.88, sample.dataMask],
@@ -18,5 +22,9 @@ function evaluatePixel(sample) {
       [0.19, 0.43, 0.11, sample.dataMask],
       [0.06, 0.33, 0.04, sample.dataMask],
       [0, 0.27, 0, sample.dataMask],
-    ])
+    ]);
+
+  return {
+    default: id_default,
+  };
 }
