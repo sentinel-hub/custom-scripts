b82fb0510239f6b83b265fc96792e437dd1b1df5
Ziga Cernigoj <ziga.cernigoj@sinergise.com>
2024-04-10 09:54:45 +0200

    PlanetScope NDWI raw values: add script

---
+++ planet_scope/ndwi/raw.js
@@ -0,0 +1,16 @@
+//VERSION=3
+//NDWI
+
+function setup() {
+  return {
+    input: [{ bands: ["green", "nir", "dataMask"] }],
+    output: [
+      { id: "default", bands: 1, sampleType: "FLOAT32" },
+    ]
+  }
+}
+
+function evaluatePixel(samples) {
+  let ndwi = index(samples.green, samples.nir);
+  return {default: [ndwi]};
+}
