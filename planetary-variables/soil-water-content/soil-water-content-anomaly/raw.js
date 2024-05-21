f3b8fd80e3714c64e5c55d99cc2e5b7f023a6c09
Jonas <53001455+jonasViehweger@users.noreply.github.com>
2024-05-21 17:14:53 +0200

    Fixed temporal aggregation and error handling on SH (#312)

--- planetary-variables/soil-water-content/soil-water-content-anomaly/raw.js
+++ planetary-variables/soil-water-content/soil-water-content-anomaly/raw.js
@@ -1,8 +1,8 @@
 // tolerance in either direction, so i.e. +- 5 days
 const toleranceDays = 1;
 
-const NODATA = NaN;
 const band = "SWC";
+const NODATA = NaN;
 
 function setup() {
     return {
@@ -34,20 +34,31 @@ function sortDatesDescending(d1, d2) {
     return date2 - date1;
 }
 
+var obsPerYear = [0];
 function preProcessScenes(collections) {
     // sort
     let scenes = collections.scenes.orbits;
+    if (scenes.length == 0) {
+        return collections;
+    }
     scenes = scenes.sort(sortDatesDescending);
     let newScenes = [];
     // convert first scene to day of year
     const observed = new Date(scenes[0].dateFrom);
     const obsMs = datetimeToYearEpoch(observed);
+    var year = 0;
     for (let i = 0; i < scenes.length; i++) {
         let currentDate = new Date(scenes[i].dateFrom);
         let sceneMs = datetimeToYearEpoch(currentDate);
         let dt = relDiff(obsMs, sceneMs);
         if (dt <= toleranceMs) {
             newScenes.push(scenes[i]);
+            obsPerYear[year]++;
+        } else {
+            if (obsPerYear[year] != 0) {
+                year++;
+                obsPerYear[year] = 0;
+            }
         }
     }
 
@@ -85,11 +96,28 @@ function std(array, mean) {
 }
 
 function evaluatePixel(samples) {
+    // Return transparent, if no value for most recent image
+    if (samples.length == 0) return [NODATA];
+    if (!samples[0].dataMask) return [NODATA];
     const values = [];
-    for (let i = samples.length; i--; ) {
-        if (samples[i].dataMask) {
-            values.push(samples[i][band]);
+    var yearIndex = 0;
+    for (let i = 0; i < obsPerYear.length; i++) {
+        let obsInYear = obsPerYear[i];
+        if (obsInYear == 0) continue;
+        let validObsInYear = 0;
+        var yearSum = 0;
+        for (let j = 0; j < obsInYear; j++) {
+            var currentIndex = yearIndex + j;
+            if (samples[currentIndex].dataMask) {
+                yearSum += samples[currentIndex][band];
+                validObsInYear++;
+            }
         }
+        if (validObsInYear) {
+            const yearMean = yearSum / validObsInYear;
+            values.push(yearMean);
+        }
+        yearIndex += obsInYear;
     }
     if (values.length === 0) return [NODATA];
     const valsMean = mean(values);
