8b6f67a99a88cec0e22e4dcb13e713e81d608a5d
Ziga Cernigoj <ziga.cernigoj@sinergise.com>
2024-06-06 11:48:02 +0200

    improve default script for skysat

--- skysat/ndvi/script.js
+++ skysat/ndvi/script.js
@@ -15,16 +15,16 @@ function evaluatePixel(sample) {
   let id_default = valueInterpolate(NDVI,
     [0, 0.2, 0.3, 0.4, 0.5, 1.0],
     [
-      [1, 1, 0.88, sample.dataMask],
-      [0.57, 0.75, 0.32, sample.dataMask],
-      [0.44, 0.64, 0.25, sample.dataMask],
-      [0.31, 0.54, 0.18, sample.dataMask],
-      [0.19, 0.43, 0.11, sample.dataMask],
-      [0.06, 0.33, 0.04, sample.dataMask],
-      [0, 0.27, 0, sample.dataMask],
+      [1, 1, 0.88],
+      [0.57, 0.75, 0.32],
+      [0.44, 0.64, 0.25],
+      [0.31, 0.54, 0.18],
+      [0.19, 0.43, 0.11],
+      [0.06, 0.33, 0.04],
+      [0, 0.27, 0],
     ]);
 
   return {
-    default: id_default,
+    default: [...id_default, sample.dataMask],
   };
 }
