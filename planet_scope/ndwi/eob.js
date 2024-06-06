645fc95e8ac0dcf3f2bea1cd8ff89611ad45dd5b
Ziga Cernigoj <ziga.cernigoj@sinergise.com>
2024-06-06 10:01:12 +0200

    rename samples to sample in PS NDWIeob script

--- planet_scope/ndwi/eob.js
+++ planet_scope/ndwi/eob.js
@@ -13,27 +13,27 @@ function setup() {
   }
 }
 
-function evaluatePixel(samples) {
-  let ndwi = (samples.Green / 3000 - samples.NIR / 3000) / (samples.Green / 3000 + samples.NIR / 3000);
-  const indexVal = samples.dataMask === 1 ? ndwi : NaN;
+function evaluatePixel(sample) {
+  let ndwi = (sample.Green / 3000 - sample.NIR / 3000) / (sample.Green / 3000 + sample.NIR / 3000);
+  const indexVal = sample.dataMask === 1 ? ndwi : NaN;
 
   let id_default = colorBlend(ndwi,
     [-1, -0.5, -0.2, 0, 0.2, 0.5, 1.0],
     [
-      [1, 0, 1, samples.dataMask],
-      [1, 0.5, 0, samples.dataMask],
-      [1, 1, 0, samples.dataMask],
-      [0.2, 1, 0.5, samples.dataMask],
-      [0, 0, 1, samples.dataMask],
-      [0, 0, 0.3, samples.dataMask],
-      [0, 0, 0, samples.dataMask],
+      [1, 0, 1, sample.dataMask],
+      [1, 0.5, 0, sample.dataMask],
+      [1, 1, 0, sample.dataMask],
+      [0.2, 1, 0.5, sample.dataMask],
+      [0, 0, 1, sample.dataMask],
+      [0, 0, 0.3, sample.dataMask],
+      [0, 0, 0, sample.dataMask],
     ]
   );
 
   return {
     default: id_default,
     index: [indexVal],
-    eobrowserStats: [indexVal, samples.dataMask],
-    dataMask: [samples.dataMask],
+    eobrowserStats: [indexVal, sample.dataMask],
+    dataMask: [sample.dataMask],
   };
 }
