4122b48595efd4e9fa5c1e2a8bac592a35b668a1
Matt Ballard <matt.ballard@planet.com>
2024-06-11 17:59:33 -0500

    use index() for ndwi

--- planet_scope/ndwi/script.js
+++ planet_scope/ndwi/script.js
@@ -1,9 +1,6 @@
 //NDWI
 
-let band1 = sample.green;
-let band2 = sample.nir;
-const denominator = band1 + band2;
-let ndwi = denominator === 0 ? NaN : (band1 - band2) / denominator;
+let ndwi = index(green, nir);
 
 return colorBlend(ndwi,
   [-1, -0.5, -0.2, 0, 0.2, 0.5, 1.0],
