a7603ce9ec3d5a4d233e3d54cca0f26834152ae1
Charlie Moriarty <charlie@planet.com>
2024-11-07 12:38:26 +0000

    Add improved formatting, align whitespace

--- planetary-variables/soil-water-content/soil-water-content-flash-drought/raw.js
+++ planetary-variables/soil-water-content/soil-water-content-flash-drought/raw.js
@@ -1,16 +1,16 @@
 //VERSION=3
 
-const nDaysBackwardAverage = 14;   // The number of days that is used to calculate the swc backward average
-const nDaysPrevious = 28; 	    // The number of days to look back to compare the current value with the previous value
-const scaleFactor = 1000;          // The scale factor for the SWC values
-const droughtThreshold = 0.15;     // The SWC value under which conditions are considered drought-like
+const nDaysBackwardAverage = 14; // The number of days that is used to calculate the swc backward average
+const nDaysPrevious = 28; // The number of days to look back to compare the current value with the previous value
+const scaleFactor = 1000; // The scale factor for the SWC values
+const droughtThreshold = 0.15; // The SWC value under which conditions are considered drought-like
 const differenceThreshold = -0.12; // Threshold signifying a rapid drop in SWC from the previous average value
 
 function setup() {
   return {
     input: ["SWC", "dataMask"],
     output: { bands: 1 },
-    mosaicking: "ORBIT"
+    mosaicking: "ORBIT",
   };
 }
 
@@ -19,11 +19,15 @@ const daysToLoad = nDaysBackwardAverage + nDaysPrevious; // The number of days l
 function preProcessScenes(collections) {
   const millisecondsBack = daysToLoad * 24 * 3600 * 1000;
 
-  collections.scenes.orbits = collections.scenes.orbits.filter(function (orbit) {
-    const orbitDateFrom = new Date(orbit.dateFrom)
-    return orbitDateFrom.getTime() >= (collections.to.getTime() - millisecondsBack);
-  })
-  return collections
+  collections.scenes.orbits = collections.scenes.orbits.filter(function (
+    orbit
+  ) {
+    const orbitDateFrom = new Date(orbit.dateFrom);
+    return (
+      orbitDateFrom.getTime() >= collections.to.getTime() - millisecondsBack
+    );
+  });
+  return collections;
 }
 
 function getMeanSWCValue(swc, dataMask) {
@@ -38,7 +42,7 @@ function getMeanSWCValue(swc, dataMask) {
   }
 
   // Calculate the mean SWC value
-  let meanSWCValue = NaN
+  let meanSWCValue = NaN;
   if (validDateCount > 0) {
     meanSWCValue = sum / validDateCount;
   }
@@ -48,21 +52,31 @@ function getMeanSWCValue(swc, dataMask) {
 
 function evaluatePixel(samples) {
   // When there are no dates, return no data
-  if (samples.length == 0) return [NaN, NaN, NaN, 0];
-  if (samples.length < daysToLoad) return [NaN, NaN, NaN, 0];
+  if (samples.length == 0) return [NaN];
+  // When the search interval of the request body is too short to perform the calculation, return no data
+  if (samples.length < daysToLoad) return [NaN];
 
   // Extract SWC values and dataMask
-  const swc = samples.slice(0, nDaysBackwardAverage).map(sample => sample.SWC / scaleFactor);
-  const dataMask = samples.slice(0, nDaysBackwardAverage).map(sample => sample.dataMask);
-  const swcPrevious = samples.slice(nDaysPrevious, nDaysBackwardAverage + nDaysPrevious).map(sample => sample.SWC / scaleFactor);
-  const dataMaskPrevious = samples.slice(nDaysPrevious, nDaysBackwardAverage + nDaysPrevious).map(sample => sample.dataMask);
+  const swc = samples
+    .slice(0, nDaysBackwardAverage)
+    .map((sample) => sample.SWC / scaleFactor);
+  const dataMask = samples
+    .slice(0, nDaysBackwardAverage)
+    .map((sample) => sample.dataMask);
+  const swcPrevious = samples
+    .slice(nDaysPrevious, nDaysBackwardAverage + nDaysPrevious)
+    .map((sample) => sample.SWC / scaleFactor);
+  const dataMaskPrevious = samples
+    .slice(nDaysPrevious, nDaysBackwardAverage + nDaysPrevious)
+    .map((sample) => sample.dataMask);
 
   // Calculate mean SWC value
   const meanSWC = getMeanSWCValue(swc, dataMask);
   const meanSWCPrevious = getMeanSWCValue(swcPrevious, dataMaskPrevious);
 
   const swcDifference = meanSWC - meanSWCPrevious;
-  const isFlashDrought = (meanSWC < droughtThreshold && swcDifference < differenceThreshold) ? 1 : 0;
+  const isFlashDrought =
+    meanSWC < droughtThreshold && swcDifference < differenceThreshold ? 1 : 0;
 
   return [isFlashDrought];
 }
--- planetary-variables/soil-water-content/soil-water-content-flash-drought/script.js
+++ planetary-variables/soil-water-content/soil-water-content-flash-drought/script.js
@@ -1,16 +1,16 @@
 //VERSION=3
 
-const nDaysBackwardAverage = 14;   // The number of days that is used to calculate the swc backward average
-const nDaysPrevious = 28; 	    // The number of days to look back to compare the current value with the previous value
-const scaleFactor = 1000;          // The scale factor for the SWC values
-const droughtThreshold = 0.15;     // The SWC value under which conditions are considered drought-like
+const nDaysBackwardAverage = 14; // The number of days that is used to calculate the swc backward average
+const nDaysPrevious = 28; // The number of days to look back to compare the current value with the previous value
+const scaleFactor = 1000; // The scale factor for the SWC values
+const droughtThreshold = 0.15; // The SWC value under which conditions are considered drought-like
 const differenceThreshold = -0.12; // Threshold signifying a rapid drop in SWC from the previous average value
 
 function setup() {
   return {
     input: ["SWC", "dataMask"],
     output: { bands: 4 },
-    mosaicking: "ORBIT"
+    mosaicking: "ORBIT",
   };
 }
 
@@ -18,12 +18,18 @@ const daysToLoad = nDaysBackwardAverage + nDaysPrevious; // The number of days l
 
 function preProcessScenes(collections) {
   const millisecondsBack = daysToLoad * 24 * 3600 * 1000;
-  collections.scenes.orbits = collections.scenes.orbits.filter(function (orbit) {
-    const orbitDateFrom = new Date(orbit.dateFrom)
-    return orbitDateFrom.getTime() >= (collections.to.getTime() - millisecondsBack);
-  })
-  return collections
+
+  collections.scenes.orbits = collections.scenes.orbits.filter(function (
+    orbit
+  ) {
+    const orbitDateFrom = new Date(orbit.dateFrom);
+    return (
+      orbitDateFrom.getTime() >= collections.to.getTime() - millisecondsBack
+    );
+  });
+  return collections;
 }
+
 function getMeanSWCValue(swc, dataMask) {
   // Get the sum of all SWC values
   let validDateCount = 0;
@@ -34,30 +40,48 @@ function getMeanSWCValue(swc, dataMask) {
       validDateCount += 1;
     }
   }
+
   // Calculate the mean SWC value
-  let meanSWCValue = NaN
+  let meanSWCValue = NaN;
   if (validDateCount > 0) {
     meanSWCValue = sum / validDateCount;
   }
+
   return meanSWCValue;
 }
+
 function evaluatePixel(samples) {
   // When there are no dates, return no data
   if (samples.length == 0) return [NaN, NaN, NaN, 0];
+  // When the search interval of the request body is too short to perform the calculation, return no data
   if (samples.length < daysToLoad) return [NaN, NaN, NaN, 0];
+
   // Extract SWC values and dataMask
-  const swc = samples.slice(0, nDaysBackwardAverage).map(sample => sample.SWC / scaleFactor);
-  const dataMask = samples.slice(0, nDaysBackwardAverage).map(sample => sample.dataMask);
-  const swcPrevious = samples.slice(nDaysPrevious, nDaysBackwardAverage + nDaysPrevious).map(sample => sample.SWC / scaleFactor);
-  const dataMaskPrevious = samples.slice(nDaysPrevious, nDaysBackwardAverage + nDaysPrevious).map(sample => sample.dataMask);
+  const swc = samples
+    .slice(0, nDaysBackwardAverage)
+    .map((sample) => sample.SWC / scaleFactor);
+  const dataMask = samples
+    .slice(0, nDaysBackwardAverage)
+    .map((sample) => sample.dataMask);
+  const swcPrevious = samples
+    .slice(nDaysPrevious, nDaysBackwardAverage + nDaysPrevious)
+    .map((sample) => sample.SWC / scaleFactor);
+  const dataMaskPrevious = samples
+    .slice(nDaysPrevious, nDaysBackwardAverage + nDaysPrevious)
+    .map((sample) => sample.dataMask);
+
   // Calculate mean SWC value
   const meanSWC = getMeanSWCValue(swc, dataMask);
   const meanSWCPrevious = getMeanSWCValue(swcPrevious, dataMaskPrevious);
+
   const swcDifference = meanSWC - meanSWCPrevious;
-  const isFlashDrought = (meanSWC < droughtThreshold && swcDifference < differenceThreshold) ? 1 : 0;
+  const isFlashDrought =
+    meanSWC < droughtThreshold && swcDifference < differenceThreshold ? 1 : 0;
+
   let opacity = 0;
   if (isFlashDrought) {
     opacity = 1;
   }
+
   return [isFlashDrought, 0, 0, opacity];
 }
